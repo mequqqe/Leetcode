6ac9a0ff6282010d9de5d9ed8f12dd6289cf7197
Daulet <daulet@qit.kz>
2023-09-21 15:24:42 +0600

    commit

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+
+        //хэшсет это это математические операции,тут HashSet<int>(nums).Count
+        //хэшсет получает сколько уникальных цифр в массвие,а тут  общее количество элемеонтом в массие  nums.Length
+        //и они медлу собой сравнивается,если уникальный элемент меньше количестов элементов в массиве то озночает что есть повторяющиеся элементы
+        return new HashSet<int>(nums).Count < nums.Length;
+    }
+}
