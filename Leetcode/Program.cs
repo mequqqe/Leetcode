94991da5a367219676399558e5e92909492c3053
Daulet <daulet@qit.kz>
2023-09-21 14:32:34 +0600

    fix():add comment

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -2,8 +2,11 @@
 {
     public bool IsPalindrome(int x)
     {
+        //Иннициализиация сһчисла в стринг
         string str = x.ToString();
+        //его "переварачиваешь"
         string reversedStr = new string(str.Reverse().ToArray());
+
         return str == reversedStr;
     }
 }
