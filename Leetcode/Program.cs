141cd3e2ce3685bbd382df2596e37acc5f0a73a6
Daulet <daulet@qit.kz>
2023-09-28 15:04:07 +0600

    Buy Two Chocolates

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,12 +1,37 @@
 ﻿public class Solution
 {
-    public bool IsPalindrome(int x)
+    public int BuyChoco(int[] prices, int money)
     {
-        //Иннициализиация сһчисла в стринг
-        string str = x.ToString();
-        //его "переварачиваешь"
-        string reversedStr = new string(str.Reverse().ToArray());
+        //сортируется массив по алгоримту вставки
+        for (int i = 1; i < prices.Length; i++)
+        {
+            var key = prices[i];
+            var j = i - 1;
+            while (j >= 0 && prices[j] > key)
+            {
+                prices[j + 1] = prices[j];
+                j--;
+            }
+            prices[j + 1] = key;
+        }
 
-        return str == reversedStr;
+        //послпе сортировки нахолим минимальный и макс значение,
+        //если сортирован то можно просто получить первый и послдений массив
+        int firstChoco = prices[0];
+        int lastChoco = prices[prices.Length - 1];
+
+        int price = firstChoco + lastChoco;
+
+
+        //тут уже  проверяете, можно ли купить эти две шоколадки,если можно то вернет остаток,
+        //если нет то просто вернет деньги
+        if (price <= money)
+        {
+            return money - price;
+        }
+        else
+        {
+            return money;
+        }
     }
 }
