e0d5ce26971065df7128dc7440d599d2013bd1eb
Daulet <daulet@qit.kz>
2023-09-29 15:13:45 +0600

    ss

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,3 +1,22 @@
-﻿public class Solution : SolutionBase
+﻿public class Solution
 {
+    public int LengthOfLastWord(string s)
+    {
+        if (s.Length == 0)
+        {
+            return 0;
+        }
+        string w = s.Trim();
+
+        string[] k = w.Split(' ');
+
+        if (k.Length == 0)
+        {
+            return 0;
+        }
+
+        string t = k[k.Length - 1];
+
+        return t.Length;
+    }
 }
