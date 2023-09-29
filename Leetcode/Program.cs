362c4dbfbad429076b5f380f738d63aff7a18db3
Daulet <daulet@qit.kz>
2023-09-29 10:13:06 +0600

    ip4 ip6

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,12 +1,3 @@
-﻿public class Solution
+﻿public class Solution : SolutionBase
 {
-    public bool IsPalindrome(int x)
-    {
-        //Иннициализиация сһчисла в стринг
-        string str = x.ToString();
-        //его "переварачиваешь"
-        string reversedStr = new string(str.Reverse().ToArray());
-
-        return str == reversedStr;
-    }
 }
