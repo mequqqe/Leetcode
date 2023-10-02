031a28ae1636d8f079e0b399a9018963960ee9f9
Daulet <daulet@qit.kz>
2023-10-02 15:47:24 +0600

    License Key Formatting

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,18 +1,61 @@
-﻿public class Solution
+﻿using System.Text;
+
+public class Solution
 {
-    public bool IsPrefixString(string s, string[] words)
+    //public string LicenseKeyFormatting(string s, int k)
+    //{
+    //    s = s.Replace("-", "").ToUpper();
+    //    int middle = s.Length % k;
+    //    int length = s.Length;
+    //    StringBuilder result = new StringBuilder();
+    //    result.Append(s.Substring(0, firstGroupLength));
+
+    //    for (int i = middle; i < length; i += k)
+    //    {
+    //        result.Append('-');
+    //        result.Append(s.Substring(i, k));
+    //    }
+
+    //    return result.ToString();
+
+    //}
+
+    //первый кейс не проходит 
+
+    public string LicenseKeyFormatting(string s, int k)
     {
-        string result = string.Empty;
+        
+        s = s.Replace("-", "").ToUpper();
 
-        foreach (string word in words)
+       
+        int length = s.Length;
+
+        //проверка на дефисы
+
+        if (length == 0)
         {
-            result += word;
-            if (result == s)
-            {
-                return true;
-            }
+            return "";
         }
-        return false;
 
+
+        int middle = length % k;
+        if (middle == 0) middle = k;
+
+        StringBuilder result = new StringBuilder();
+
+        //проверка на не ноль
+        if (middle > 0)
+        {
+            result.Append(s.Substring(0, middle));
+        }
+
+        result.Append(s.Substring(0, middle));
+        for (int i = middle; i < length; i += k)
+        {
+            result.Append('-');
+            result.Append(s.Substring(i, k));
+        }
+
+        return result.ToString();
     }
 }
