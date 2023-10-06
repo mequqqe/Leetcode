a0881845455f0c5c4a68b610386346f08fa110f0
Daulet <daulet@qit.kz>
2023-10-06 13:42:18 +0600

    cc

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int FindClosestNumber(int[] nums)
+    {
+        int closestNumber = nums[0];
+
+        foreach (int num  in nums)
+        {
+            //Math.abs вернет число абсолютным,если -3 то 3,тут проверка на ближнее число к нулю и каждый раз он обновляет его,
+            //Если какое-либо число ближе к нулю, чем текущее,то оно становится новым ближним числом к нулю
+            if (Math.Abs(num) < Math.Abs(closestNumber) || (Math.Abs(num) == Math.Abs(closestNumber) && num > closestNumber))
+            {
+                closestNumber = num;
+            }
+        }
+    }
+}
