12241be3b6fc18ed808ec737a08b2d97a9126882
Daulet <daulet@qit.kz>
2023-10-16 10:53:17 +0600

    ss

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                dict[num]++;
+            }
+            else
+            {
+                dict[num] = 1;
+            }
+
+            if (dict[num] > nums.Length / 2)
+            {
+                return num;
+            }
+        }
+
+        return 0; 
+    }
+}
